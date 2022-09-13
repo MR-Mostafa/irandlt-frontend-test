b168b9ab7183a3d0b7e59d30ce6fc87111a7c53d
Mostafa Rahmati <M.Rahmati70@hotmail.com>
2022-09-13 14:17:55 +0430

    update next type

--- types/nextType.d.ts
+++ types/nextType.d.ts
@@ -1,4 +1,6 @@
+import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
+import type { ReactElement, ReactNode } from 'react';
 
 export type NextPageWithLayout = NextPage & {
 	getLayout?: (page: ReactElement) => ReactNode;
