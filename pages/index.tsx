62e8c0d08a81e3527d2516a62674917f435689fd
Mostafa Rahmati <M.Rahmati70@hotmail.com>
2022-09-13 14:18:32 +0430

    update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,23 @@
 import type { NextPage } from 'next';
+import Link from 'next/link';
 
-import { Container } from '@mantine/core';
-
-import Header from '~root/components/Header/Header';
+import { Box, Button } from '@mantine/core';
 
 const Home: NextPage = () => {
 	return (
-		<Container fluid>
-			<Header />
-		</Container>
+		<>
+			<Box sx={{ textAlign: 'center' }}>
+				<Box component="p" py={15}>
+					جهت مشاهده صفحه پرواز بر روی لینک مربوطه کلیک نمایید.
+				</Box>
+
+				<Link href="/flight" passHref>
+					<Button component="a" variant="gradient" gradient={{ from: 'teal', to: 'lime', deg: 105 }} size="lg" radius="lg">
+						ورود به صفحه جستجو پرواز
+					</Button>
+				</Link>
+			</Box>
+		</>
 	);
 };
 
