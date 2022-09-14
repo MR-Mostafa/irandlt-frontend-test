f417db94155ca501bf8c17492262b15ca3280c64
Mostafa Rahmati <M.Rahmati70@hotmail.com>
2022-09-14 15:58:42 +0430

    global constants

---
+++ constants/globalConstants.ts
@@ -0,0 +1,9 @@
+export const _OneSecond = 1000;
+
+export const _MinInMs = 60 * _OneSecond;
+
+export const _DayInMs = 24 * 60 * _MinInMs;
+
+export const _CachedResponse = _MinInMs * 10; // 10 min
+
+export const _CachedRevalidate = _MinInMs * 15; // 15 min
