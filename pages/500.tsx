d27724d506d01ef28d9d1a7eb63d4b363068f65d
Mostafa Rahmati <m.rahmati70@hotmail.com>
2022-09-16 21:29:28 +0430

    500 page

---
+++ pages/500.tsx
@@ -0,0 +1,44 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+import { Box, Button, Text } from '@mantine/core';
+
+const Page404 = () => {
+	return (
+		<>
+			<Head>
+				<title>تست فرانت‌اند | یافت نشد</title>
+			</Head>
+
+			<Text align="center" size={60} pt={20} sx={{ fontWeight: 'bold', lineHeight: '1.6' }}>
+				500
+			</Text>
+
+			<Text align="center" size={25} sx={{ fontWeight: 'bold' }}>
+				خطای ناشناخته.
+			</Text>
+
+			<Box sx={{ textAlign: 'center' }} py={20}>
+				<Link href="/" passHref>
+					<Button
+						component="a"
+						color="dark"
+						radius={60}
+						size="md"
+						styles={{
+							root: {
+								'&:hover': {
+									color: 'rgba(256,256,256,0.8)',
+								},
+							},
+						}}
+					>
+						صفحه اصلی
+					</Button>
+				</Link>
+			</Box>
+		</>
+	);
+};
+
+export default Page404;
