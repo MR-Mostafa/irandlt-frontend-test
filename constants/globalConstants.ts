c23beb7d312638212a213384a6ccbfd255169000
Mostafa Rahmati <m.rahmati70@hotmail.com>
2022-09-16 21:29:18 +0430

    _DateRegex & _CityValueRegex

--- constants/globalConstants.ts
+++ constants/globalConstants.ts
@@ -7,3 +7,11 @@ export const _DayInMs = 24 * 60 * _MinInMs;
 export const _CachedResponse = _MinInMs * 10; // 10 min
 
 export const _CachedRevalidate = _MinInMs * 15; // 15 min
+
+/**
+ * The reason of deleting "g" flag in regex
+ * {@tutorial https://dev.to/dvddpl/why-is-my-regex-working-intermittently-4f4g}
+ */
+export const _DateRegex = /^20[0-9]{2}-[0-9]{1,2}-[0-9]{1,2}$/i;
+
+export const _CityValueRegex = /^[a-z]{3}$/i;
